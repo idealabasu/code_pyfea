b1a4a7f4e8414478bb9f19dd225c31ca7c8e010f
Dan Aukes <danaukes@gmail.com>
2020-04-22 12:04:11 -0700

    updated mesh tools

--- python/pyfea_examples/box_general_gmsh.py
+++ python/pyfea_examples/box_general_gmsh.py
@@ -26,67 +26,12 @@ def create_t(points,quads):
     dqp = quad_points[1:] - quad_points[0]
 
 
-import os
-
-
-def generate_mesh():
-    geom = pg.opencascade.Geometry(characteristic_length_max=.25, characteristic_length_min=.1)
-    geom.add_box((0,0,0),(10,10,.5))
-    mo = pg.generate_mesh(geom)
-    return mo
-
-
-
-def mesh_to_dict(mo):
-    mo2 = {}
-    mo2['cell_data'] = mo.cell_data
-    mo2['cells'] = {}
-    for key,value in mo.cells.items():
-        mo2['cells'][key] = mo.cells[key].tolist(),mo.cells[key].dtype
-    mo2['field_data'] = mo.field_data
-    mo2['gmsh_periodic'] = mo.gmsh_periodic
-    mo2['info'] = mo.info
-    mo2['node_sets'] = mo.node_sets
-    mo2['point_data'] = mo.point_data
-    mo2['points'] = mo.points.tolist(),mo.points.dtype
-    return mo2    
-
-def save_mesh(mo,meshname):
-    #mo2['prune']
-    mo2 = mesh_to_dict(mo)
-    
-    with open(meshname,'w') as f:
-        yaml.dump(mo2,f)
-    
-def load_mesh(meshname):
-    with open(meshname) as f:
-        mo2 = yaml.load(f)
-    cells = {}
-    for key,value in mo2['cells'].items():
-        cells[key] = numpy.array(*value)
-    mo2['cells'] = cells
-    mo2['points'] = numpy.array(*(mo2['points']))
-    mo = GenericData(**mo2)
-    return mo
-
+import pyfea.mesh as pm
 
 meshname = 'plate.yaml'
 #mo=load_mesh(meshname)
-if os.path.exists(meshname):
-    try:
-        mo = load_mesh(meshname)
-    except Exception:
-        mo=generate_mesh()
-        save_mesh(mo,meshname)
-else:
-    mo=generate_mesh()
-    save_mesh(mo,meshname)
     
-    
-
-
-
-
+mo = pm.get_mesh(meshname,pm.generate_plate)
 
 triangles_outer = mo.cells['triangle']
 
