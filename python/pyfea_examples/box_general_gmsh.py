75776614d2cae005e3c850a3f86adec2bb85fed1
Dan Aukes <danaukes@gmail.com>
2020-04-22 11:56:32 -0700

    updated general box

--- python/pyfea_examples/box_general_gmsh.py
+++ python/pyfea_examples/box_general_gmsh.py
@@ -10,6 +10,7 @@ import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import yaml
+from idealab_tools.data_exchange.generic_data import GenericData
 
 import pyfea.fea as fea
 import idealab_tools.matplotlib_tools
@@ -34,17 +35,43 @@ def generate_mesh():
     mo = pg.generate_mesh(geom)
     return mo
 
+
+
+def mesh_to_dict(mo):
+    mo2 = {}
+    mo2['cell_data'] = mo.cell_data
+    mo2['cells'] = {}
+    for key,value in mo.cells.items():
+        mo2['cells'][key] = mo.cells[key].tolist(),mo.cells[key].dtype
+    mo2['field_data'] = mo.field_data
+    mo2['gmsh_periodic'] = mo.gmsh_periodic
+    mo2['info'] = mo.info
+    mo2['node_sets'] = mo.node_sets
+    mo2['point_data'] = mo.point_data
+    mo2['points'] = mo.points.tolist(),mo.points.dtype
+    return mo2    
+
 def save_mesh(mo,meshname):
+    #mo2['prune']
+    mo2 = mesh_to_dict(mo)
+    
     with open(meshname,'w') as f:
-        yaml.dump(mo,f)
+        yaml.dump(mo2,f)
     
 def load_mesh(meshname):
     with open(meshname) as f:
-        mo = yaml.load(f)
+        mo2 = yaml.load(f)
+    cells = {}
+    for key,value in mo2['cells'].items():
+        cells[key] = numpy.array(*value)
+    mo2['cells'] = cells
+    mo2['points'] = numpy.array(*(mo2['points']))
+    mo = GenericData(**mo2)
     return mo
 
 
 meshname = 'plate.yaml'
+#mo=load_mesh(meshname)
 if os.path.exists(meshname):
     try:
         mo = load_mesh(meshname)
@@ -54,6 +81,11 @@ if os.path.exists(meshname):
 else:
     mo=generate_mesh()
     save_mesh(mo,meshname)
+    
+    
+
+
+
 
 
 triangles_outer = mo.cells['triangle']
